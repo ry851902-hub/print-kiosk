2a5a338a6f88e7bbe10096f8ef82b43e285b3410
ry851902-hub <ry851902@gmail.com>
2026-05-24 02:21:05 +0530

    AdminPage

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-const API_BASE = 'http://localhost:5000'
+const API_BASE = 'http://paternal-disorder-viper.ngrok-free.dev'
 
 export default function AdminPage() {
   const [signalSent, setSignalSent] = useState(false)
